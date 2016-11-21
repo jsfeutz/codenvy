c9eea7e6923efc0c5276d33a568d0595f2cf837e
Ann Shumilova <ashumilova@codenvy.com>
2016-11-21 11:47:49 +0200

    CODENVY-803: ability to create new team and edit it's details
    Signed-off-by: Ann Shumilova <ashumilova@codenvy.com>

--- dashboard/src/components/api/codenvy-permissions.factory.ts
+++ dashboard/src/components/api/codenvy-permissions.factory.ts
@@ -22,14 +22,46 @@
 export class CodenvyPermissions {
 
   /**
+   * Angular promise service.
+   */
+  private $q: ng.IQService;
+
+  /**
+   * Angular Resource service.
+   */
+  private $resource: ng.resource.IResourceService;
+  /**
+   * Services availability to user.
+   */
+  private userServices: any;
+  /**
+   * Workspace permissions with workspace's id as a key.
+   */
+  private workspacePermissions: Map<string, any>;
+  /**
+   * Team permissions with team's id as a key.
+   */
+  private teamPermissions: Map<string, any>;
+  /**
+   * Available system permissions.
+   */
+  private systemPermissions: any;
+
+  /**
+   * Client to make remote permissions API calls.
+   */
+  private remotePermissionsAPI: ng.resource.IResourceClass<ng.resource.IResource<any>>;
+
+  /**
    * Default constructor that is using resource
    * @ngInject for Dependency injection
    */
-  constructor($q, $resource) {
+  constructor($q: ng.IQService, $resource: ng.resource.IResourceService) {
     this.$q = $q;
     this.$resource = $resource;
 
     this.workspacePermissions = new Map();
+    this.teamPermissions = new Map();
     this.systemPermissions = null;
 
     this.userServices = {
@@ -40,7 +72,7 @@ export class CodenvyPermissions {
       hasLicenseService: false
     };
 
-    // remote call
+    // remote permissions calls
     this.remotePermissionsAPI = this.$resource('/api/permissions', {}, {
       store: {method: 'POST', url: '/api/permissions'},
       remove: {method: 'DELETE', url: '/api/permissions/:domain?instance=:instance&user=:user'},
@@ -53,20 +85,57 @@ export class CodenvyPermissions {
    * Stores permissions data.
    *
    * @param data - permissions data
-   * @returns {*}
+   * @returns {ng.IPromise<any>}
    */
-  storePermissions(data) {
+  storePermissions(data: any): ng.IPromise<any> {
     let promise = this.remotePermissionsAPI.store(data).$promise;
     return promise;
   }
 
   /**
-   * Fetch workspace permissions
+   * Fetch team permissions by team's id.
+   *
+   * @param teamId team id
+   * @returns {ng.IPromise<any>}
+   */
+  fetchTeamPermissions(teamId: string): ng.IPromise<any> {
+    let promise = this.remotePermissionsAPI.getPermissionsByInstance({domain: 'organization', instance: teamId}).$promise;
+    let resultPromise = promise.then((permissions) => {
+      this.teamPermissions.set(teamId, permissions);
+    });
+
+    return resultPromise;
+  }
+
+  /**
+   * Returns the list of team's permissions by team's id
+   *
+   * @param teamId team id
+   * @returns {*} list of team permissions
+   */
+  getTeamPermissions(teamId: string): any {
+    return this.teamPermissions.get(teamId);
+  }
+
+  /**
+   * Remove permissions for pointed user in pointed team.
+   *
+   * @param teamId team id
+   * @param userId user id
+   * @returns {ng.IPromise<any>} request promise
+   */
+  removeTeamPermissions(teamId: string, userId: string): ng.IPromise<any> {
+    let promise = this.remotePermissionsAPI.remove({domain: 'organization', instance: teamId, user: userId}).$promise;
+    return promise;
+  }
+
+  /**
+   * Fetch workspace permissions by workspace's id.
    *
    * @param workspaceId workspace id
-   * @returns {*}
+   * @returns {ng.IPromise<any>}
    */
-  fetchWorkspacePermissions(workspaceId) {
+  fetchWorkspacePermissions(workspaceId: string): ng.IPromise<any> {
     let promise = this.remotePermissionsAPI.getPermissionsByInstance({domain: 'workspace', instance: workspaceId}).$promise;
     let resultPromise = promise.then((permissions) => {
         this.workspacePermissions.set(workspaceId, permissions);
@@ -79,37 +148,32 @@ export class CodenvyPermissions {
    * Returns permissions data by workspace id
    *
    * @param workspaceId workspace id
-   * @returns {*} list of workspace permissions
+   * @returns {ng.IPromise<any>} list of workspace permissions
    */
-  getWorkspacePermissions(workspaceId) {
+  getWorkspacePermissions(workspaceId: string): any {
     return this.workspacePermissions.get(workspaceId);
   }
 
   /**
-   * Remove permission for pointed user in pointed workspace.
+   * Remove permissions for pointed user in pointed workspace.
    *
    * @param workspaceId workspace id
    * @param userId user id
-   * @returns {$promise} request promise
+   * @returns {ng.IPromise<any>e} request promise
    */
-  removeWorkspacePermissions(workspaceId, userId) {
+  removeWorkspacePermissions(workspaceId: string, userId: string): ng.IPromise<any> {
     let promise = this.remotePermissionsAPI.remove({domain: 'workspace', instance: workspaceId, user: userId}).$promise;
     return promise;
-    let resultPromise = promise.then(() => {
-        this.fetchWorkspacePermissions(workspaceId);
-    });
-
-    return resultPromise;
   }
 
   /**
    * Fetch system permissions
-   * 
-   * @returns {*}
+   *
+   * @returns {ng.IPromise<any>}
    */
-  fetchSystemPermissions() {
+  fetchSystemPermissions(): ng.IPromise<any> {
     let promise = this.remotePermissionsAPI.getSystemPermissions().$promise;
-    let resultPromise = promise.then((systemPermissions) => {
+    let resultPromise = promise.then((systemPermissions: any) => {
       this._updateUserServices(systemPermissions);
       this.systemPermissions = systemPermissions;
     });
@@ -117,7 +181,7 @@ export class CodenvyPermissions {
     return resultPromise;
   }
 
-  _updateUserServices(systemPermissions) {
+  _updateUserServices(systemPermissions: any): void {
     let isManageUsers = systemPermissions && systemPermissions.actions.includes('manageUsers');
     let isManageCodenvy = systemPermissions && systemPermissions.actions.includes('manageCodenvy');
 
@@ -128,11 +192,11 @@ export class CodenvyPermissions {
     this.userServices.hasLicenseService = isManageCodenvy;
   }
 
-  getSystemPermissions() {
+  getSystemPermissions(): any {
     return this.systemPermissions;
   }
 
-  getUserServices() {
+  getUserServices(): any {
     return this.userServices;
   }
 }
